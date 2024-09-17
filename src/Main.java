432a064ed64bc218bf246340722de8c0f6e8fb00
esther-nadiyama <esther.nadiyama@stu.ocsb.ca>
2024-09-17 14:38:11 -0400

    changed the plot

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		/*
 		 * MadLibs Program
-		 * Author: 
+		 * Author: Esther
 		 * Class: ICS3U
 		 * Date: Sept. 16, 2024
 		 * Make a program that asks for 10 inputs
@@ -13,11 +13,21 @@ public class Main {
 		 */
 		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Welcome to Mad Libs!");
-		System.out.println("What's your name?");
-		String name = in.nextLine();
-		System.out.println("One day, "+name+" wrote a Mad Lib in class!");
+		System.out.println("Give me a number");
+		int 'num1' = in.nextLine();
+		System.out.println("Give me a sport");
+		String sport = in.nextLine();
+		System.out.print("Give me a name");
+		String name =in.nextLine();
+		System.out.print("Give me a place:");
+		//flush
+		in.nextInt();
+		String place =in.nextLine();
+		System.out.println("One day"+name+" "+num1+"  had so much fun "+place+"!");
 
+		
+		
+		
 	}
 
 }
