c4e4c207a38394466ee9cbb93e56b1358f6e83f9
esther-nadiyama <esther.nadiyama@stu.ocsb.ca>
2024-09-18 14:38:38 -0400

    system changed

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,27 @@ public class Main {
 		 */
 		
 		Scanner in = new Scanner(System.in);
-		System.out.println("Give me a number");
-		int 'num1' = in.nextLine();
-		System.out.println("Give me a sport");
-		String sport = in.nextLine();
-		System.out.print("Give me a name");
-		String name =in.nextLine();
-		System.out.print("Give me a place:");
-		//flush
-		in.nextInt();
+		System.out.println("Give me a number: ");
+		String num1 = in.nextLine();
+		System.out.println("Give me a sport: ");
+		String sport =in.nextLine();
+		System.out.print("Give me a place: ");
 		String place =in.nextLine();
-		System.out.println("One day"+name+" "+num1+"  had so much fun "+place+"!");
+		System.out.print("Give me a name: ");
+		
+		System.out.print("Give me another name: ");
+		String name =in.nextLine();
+		
+		
+		int score =in.nextInt();
+		
+		
+		
+		
+		String person2;
+		String person1;
+		String points;
+		System.out.println("One day"+person1+"  and "+person2+"  had so much fun while playing"+sport+" at "+place+" and scored "+num1+"+points+"!");
 
 		
 		
